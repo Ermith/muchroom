b71a895b8f0e3f55132581403054c89b5f733bd7
pali <6pali6@gmail.com>
2024-03-11 21:24:55 +0100

    bluh

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,6 @@ fn main() {
                 prevent_default_event_handling: false,
                 resizable: true,
                 //resolution: WindowResolution::new(1280.0, 720.0),
-                #[cfg(not(target_arch = "wasm32"))]
                 resolution: WindowResolution::new(1920.0, 1080.0),
                 ..default()
             }),
